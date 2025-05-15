ec5d4a4d3a1694678f3f55ff2925cba69ea4f8fc
koni24 <konisha.24.kar@gmail.com>
2025-05-15 18:34:48 +0530

    Initial project setup with Spring Boot and dependencies

---
+++ src/test/java/com/projects/tic_tac_toe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.tic_tac_toe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
